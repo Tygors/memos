4942e790fde6e478fc6d72cb4287f0067d5b8661
Tygors <tygors@users.noreply.github.com>
2026-06-23 16:12:29 +0800

    fix: disable S3 checksum calculation for MinIO compatibility

--- internal/storage/s3/s3.go
+++ internal/storage/s3/s3.go
@@ -44,8 +44,8 @@ func NewClient(ctx context.Context, s3Config *storepb.StorageS3Config) (*Client,
 	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
 		o.BaseEndpoint = aws.String(s3Config.Endpoint)
 		o.UsePathStyle = s3Config.UsePathStyle
-		o.RequestChecksumCalculation = aws.RequestChecksumCalculationWhenRequired
-		o.ResponseChecksumValidation = aws.ResponseChecksumValidationWhenRequired
+		o.RequestChecksumCalculation = aws.RequestChecksumCalculationWhenOptional
+		o.ResponseChecksumValidation = aws.ResponseChecksumValidationWhenOptional
 	})
 	return &Client{
 		Client: client,
